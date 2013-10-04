0a6f4a7a20a8118ba9f8c6cabeaad13af2c4c6f2
Comptly <dillon.compton@mulesoft.com>
2013-10-04 16:21:26 -0700

    updated with white background due to usability concerns. Still some issues with visual ties to code, more soon.

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -2,28 +2,16 @@ $(document).ready(function() {
     $(window).scroll(function() {
         var scrollVal = $(this).scrollTop();
         if (scrollVal > 220) {
-        /* $('.cta-mini').fadeIn('fast'); */
+        $('.cta-mini').fadeIn('fast');
             /* $('.banner-wrapper').css({'position': 'fixed','top': '-1px','z-index': '3'});
             $('.content-wrapper').css({'margin-top': '216px'});
             $('.content-wrapper-pages aside').css({'position': 'fixed','top': '0'}); */
         } else {
-            /* $('.cta-mini').fadeOut('fast'); */
+            $('.cta-mini').fadeOut('fast');
             /* $('.banner-wrapper').css({'position': 'static','top': 'auto','z-index': 'auto'});
             $('.content-wrapper').css({'margin-top': 'auto'});
             $('.content-wrapper-pages aside').css({'position': 'static','top': 'auto'}); */
         }
-    });
-    /* Code screenshot swapping on hover */
-    $('img.code-showall').delay(400).fadeIn('slow',function() {
-        $('img.code-inactive').delay(400).fadeIn();
-    });
-    $('.code-highlight').delay(1500).queue(function() {
-        $('.code-highlight.code-1').addClass('active');
-        $(this).addClass('inactive');
-    });
-    $('.code-highlight').hover(function() {
-        $('.code-highlight').removeClass('active');
-        $(this).addClass('active');
     });
     /* Apply active class to nav item on scroll */
     var sections = $(".content-wrapper-pages h2");
@@ -41,7 +29,7 @@ $(document).ready(function() {
             navigation_links.removeClass("active");
             active_link.addClass("active");
         },
-        offset: 92
+        offset: 91
     });
     /* Anchor link smooth scrolling */
     $(function() {
@@ -51,7 +39,7 @@ $(document).ready(function() {
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
                 if (target.length) {
                     $('html,body').animate({
-                        scrollTop: target.offset().top - 92
+                        scrollTop: target.offset().top - 91
                     }, 400);
                     return false;
                 }
